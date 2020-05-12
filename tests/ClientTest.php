7fe13a46976920940be4dc170656e462fb773d25
Alexandre <contact@alexandre.business>
2020-05-12 17:03:49 +0200

    #1 Be able to use app auth
    Basics of authenficiation from the library

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -14,21 +14,21 @@ class ClientTest extends TestCase
 
         // Record requests and responses in cassette file 'example'
         \VCR\VCR::insertCassette('clienttest_api');
- 
+
         // Following request will be recorded once and replayed in future test runs
         $client = new Client();
         Client::create(array(
-             'app_id' => 'e0611906',
-             'password' => 'sebfie',
-             'live' => false
+            'app_id' => 'e0611906',
+            'password' => 'sebfie',
+            'live' => false
         ));
         $result = $client->signIn();
         var_dump($result);
         $this->assertNotEmpty($result);
- 
+
         // To stop recording requests, eject the cassette
         \VCR\VCR::eject();
- 
+
         // Turn off VCR to stop intercepting requests
         \VCR\VCR::turnOff();
     }
