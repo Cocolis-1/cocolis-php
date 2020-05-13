1e5df16f1013ddffa035c4a0fcb5290cb265ddeb
Sebastien Fieloux <sebastien.fieloux@gmail.com>
2020-05-13 10:37:31 +0200

    Fix test and annotate tests to be done for alexandre

--- src/Client.php
+++ src/Client.php
@@ -12,117 +12,132 @@ namespace Cocolis\Api;
 
 class Client
 {
-    // Local informations
-    private static $_app_id;
-    private static $_password;
-    private static $_live;
+  // Local informations
+  private static $_app_id;
+  private static $_password;
+  private static $_live;
 
-    private static $_client;
+  private static $_client;
+  private static $_http_client;
 
-    // Returned by the API
-    private $_access_token;
-    private $_expiry;
-    private $_uid;
+  // Returned by the API
+  private $_access_token;
+  private $_expiry;
+  private $_uid;
 
-    const API_SANDBOX = "https://sandbox-api.cocolis.fr/api/v1/"; //  Test environment during your implementation
-    const API_PROD = "https://api.cocolis.fr/api/v1/"; // Online environment (in production, be careful what you do with this)
+  const API_SANDBOX = "https://sandbox-api.cocolis.fr/api/v1/"; //  Test environment during your implementation
+  const API_PROD = "https://api.cocolis.fr/api/v1/"; // Online environment (in production, be careful what you do with this)
 
+  public static function isLive()
+  {
+    return self::$_live;
+  }
 
-    public static function isLive()
-    {
-        return self::$_live;
+  public static function getAppId()
+  {
+    return self::$_app_id;
+  }
+
+  public static function getPassword()
+  {
+    return self::$_password;
+  }
+
+  public static function getAccessToken()
+  {
+    return self::$_access_token;
+  }
+
+  public static function getClient(array $auth)
+  {
+    if (!static::$_client) {
+        static::$_client = static::create($auth);
     }
 
-    public static function getAppId()
-    {
-        return self::$_app_id;
+    return static::$_client;
+  }
+
+  public function getHttpClient()
+  {
+    return static::$_http_client;
+  }
+
+  public static function setAppId(string $app_id)
+  {
+    self::$_app_id = $app_id;
+  }
+
+
+  public static function setPassword(string $password)
+  {
+    self::$_password = $password;
+  }
+
+  public static function setLive(bool $live)
+  {
+    self::$_live = $live;
+  }
+
+  public static function setHttpClient($http_client)
+  {
+    self::$_http_client = $http_client;
+  }
+
+  public static function setClient($client)
+  {
+    self::$_client = $client;
+  }
+
+  public function getExpiry()
+  {
+    return $this->_expiry;
+  }
+
+  public function getUID()
+  {
+    return $this->_uid;
+  }
+
+  // Initialize the connection to the api
+  public static function create(array $auth)
+  {
+    $client = new static();
+
+    // Set array of values for the class
+    if (isset($auth['app_id'])) {
+      self::setAppId($auth['app_id']);
     }
 
-    public static function getPassword()
-    {
-        return self::$_password;
+    if (isset($auth['password'])) {
+      self::setPassword($auth['password']);
     }
 
-    public static function getAccessToken()
-    {
-        return self::$_access_token;
+    if (isset($auth['live'])) {
+      self::setLive($auth['live']);
+    }
+    $url = self::isLive() ? self::API_PROD : self::API_SANDBOX;
+
+    self::setHttpClient(new \GuzzleHttp\Client(['base_uri' => $url]));
+    self::setClient($client);
+
+    return $client;
+  }
+
+  // Connect to the API
+  public function signIn()
+  {
+    $res = $this->call('app_auth/sign_in', 'POST', ['app_id' => self::getAppId(), 'password' => self::getPassword()]);
+
+    if ($res->getStatusCode() == 200) {
+      return array('access-token' => array_values($res->getHeader('Access-Token'))[0], 'client' => array_values($res->getHeader('Client'))[0], 'expiry' => array_values($res->getHeader('Expiry'))[0], 'uid' => array_values($res->getHeader('Uid'))[0]);
     }
 
-    public static function getHttpClient()
-    {
-        return self::$_client;
-    }
+    return false;
+  }
 
-    public static function setAppId(string $app_id)
-    {
-        self::$_app_id = $app_id;
-    }
-
-
-    public static function setPassword(string $password)
-    {
-        self::$_password = $password;
-    }
-
-    public static function setLive(bool $live)
-    {
-        self::$_live = $live;
-    }
-
-    public static function setClient($client)
-    {
-        self::$_client = $client;
-    }
-
-    public function getExpiry()
-    {
-        return $this->_expiry;
-    }
-
-    public function getUID()
-    {
-        return $this->_uid;
-    }
-
-
-    // Initialize the connection to the api
-    public static function create(array $auth)
-    {
-        // Set array of values for the class
-        if (isset($auth['app_id'])) {
-            self::setAppId($auth['app_id']);
-        }
-
-        if (isset($auth['password'])) {
-            self::setPassword($auth['password']);
-        }
-
-        if (isset($auth['live'])) {
-            self::setLive($auth['live']);
-        }
-
-        if (self::isLive()) {
-            self::setClient(new \GuzzleHttp\Client(['base_uri' => self::API_PROD]));
-        } else {
-            self::setClient(new \GuzzleHttp\Client(['base_uri' => self::API_SANDBOX]));
-        }
-    }
-
-    // Connect to the API
-    public function signIn()
-    {
-        $res = $this->call('app_auth/sign_in', 'POST', ['app_id' => self::getAppId(), 'password' => self::getPassword()]);
-
-        if ($res->getStatusCode() == 200) {
-            return array('access-token' => array_values($res->getHeader('Access-Token'))[0], 'client' => array_values($res->getHeader('Client'))[0], 'expiry' => array_values($res->getHeader('Expiry'))[0], 'uid' => array_values($res->getHeader('Uid'))[0]);
-        }
-
-        return false;
-    }
-
-    public function call($url, $method = 'GET', $body = array())
-    {
-        $client = $this->getHttpClient();
-        return $client->request($method, $url, ['json' => $body]);
-    }
+  public function call($url, $method = 'GET', $body = array())
+  {
+    $client = $this->getHttpClient();
+    return $client->request($method, $url, ['json' => $body]);
+  }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -7,7 +7,40 @@ use Cocolis\Api\Client;
 
 class ClientTest extends TestCase
 {
-    public function testAPI()
+    /**
+     * @before
+     */
+    public function setupSomeFixtures()
+    {
+        var_dump('I am before test');
+    }
+
+    // @TODO Test all getters, setter
+    // Getter should return value setted
+    // Setters should set values
+
+    public function getContant()
+    {
+        //@TODO API_SANDBOX should be equal to https://sandbox-api.cocolis.fr/api/v1/
+        //@TODO API_PROD should be equal to https://api.cocolis.fr/api/v1/
+    }
+
+    public function getClient()
+    {
+        //@TODO Should create a client with passed data
+        //@TODO Should return existing client is already created
+    }
+
+    public function testSignIn()
+    {
+        //@TODO Should call $client->call with right arguments
+        // Read : https://phpunit.readthedocs.io/en/9.1/test-doubles.html?highlight=mock
+
+        // @TODO Should return false if the response is not 200
+        // @TODO Should return array with right values if response is 200
+    }
+
+    public function testStaticCreate()
     {
         // After turning on the VCR will intercept all requests
         \VCR\VCR::turnOn();
@@ -16,15 +49,27 @@ class ClientTest extends TestCase
         \VCR\VCR::insertCassette('clienttest_api');
 
         // Following request will be recorded once and replayed in future test runs
-        $client = new Client();
-        Client::create(array(
+        $client = Client::create(array(
             'app_id' => 'e0611906',
             'password' => 'sebfie',
             'live' => false
         ));
         $result = $client->signIn();
-        var_dump($result);
+
         $this->assertNotEmpty($result);
+
+        // @TODO
+        // Vérifier que la réponse est bien égale à (ce que tu as dans la cassette) :
+        // array(4) {
+        //     'access-token' =>
+        //     string(22) "8dX5MJhZ6I5NAxK0-uxLjw"
+        //     'client' =>
+        //     string(22) "q5ubXwodRenhrX07axaOTA"
+        //     'expiry' =>
+        //     string(10) "1590568115"
+        //     'uid' =>
+        //     string(8) "e0611906"
+        //   }
 
         // To stop recording requests, eject the cassette
         \VCR\VCR::eject();
